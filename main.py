940a2391b58403e033e779b9ed3749f24f7a806a
Tim <tc@timothyclark.co.uk>
2020-03-12 13:22:29 +0000

    Downloading images

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 from bs4 import BeautifulSoup
+from os import path
+from pathlib import Path
+import requests
+
+BASE_FOLDER = "res/"
+
+def download_images(soup):
+    for img in soup.find_all("img"):
+        src = img.get("src")
+        if img and src:
+            print("Downloading Image: ", src)
+            # img["src"] = "new url"
+            download_resource("images", src)
+
+def download_resource(directory, url):
+    # Deal with base folder
+    if path.exists(BASE_FOLDER) and path.isdir(BASE_FOLDER):
+        print("Base folder exists at", BASE_FOLDER)
+    else:
+        print("Creating base folder:", BASE_FOLDER)
+    
+    # Check for directory
+    directory = path.join(BASE_FOLDER, directory)
+    if path.exists(directory) and path.isdir(directory):
+        print(directory, "exists")
+    else: # Doesn't exist, create
+        print("Creating folder:", directory)
+        Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # Download resource
+    res = requests.get(url)
+    dst = path.join(directory, "image.jpg")
+    if not path.exists(dst):
+        with open(dst, "wb") as f:
+            res.raw.decode_content = True
+            f.write(res.content)
+    else:
+        print("File already exists at", dst, "- skipping")
+
 
 def read_html(file_name="index.html"):
     with open(file_name, "r") as f:
@@ -8,7 +47,7 @@ def read_html(file_name="index.html"):
 
 def run(html):
     soup = BeautifulSoup(html, "html.parser")
-    print(soup.prettify())
+    download_images(soup)
 
 
 html = read_html()
